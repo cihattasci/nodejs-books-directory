6798a5ba3aeafb8d46188b80e741370a4ebf212f
cihattascigit config --global user.email cihattasci97@gmail.comgit config --global user.name cihattasci <63815355+cihattasci@users.noreply.github.com>
2020-10-29 13:08:31 +0300

    create controller and apply on route

---
+++ api/controllers/bookDirectoryController.js
@@ -0,0 +1,73 @@
+const Book = require('../models/bookDirectoryModel');
+
+const list_all_book = async (req, res) => {
+
+    //sorting
+    /*await Book.find({}).sort({pageNumber: -1}).exec((error, docs) => {
+        if(error){
+            return res.send(error);
+        };
+        res.send(docs);
+    });*/
+
+    //filtering
+    /*var title = req.query.title; 
+    var author = req.query.author;
+    await Book.find({title, author}).then(books => res.send(books)).catch(e => res.send(e));*/
+
+    //limit
+    /*var limit = parseInt(req.query.limit);
+    await Book.find({}).limit(limit).then(books => res.send(books)).catch(e => res.send(e));*/
+
+    await Book.find({}).then(books => res.send(books)).catch(e => res.send(e));
+};
+
+const save_book = async (req, res) => {
+
+    const book = new Book({
+        title: req.body.title,
+        author: req.body.author,
+        pageNumber: req.body.pageNumber
+    });
+
+    await book.save((error, result) => {
+
+        if (error){
+            return res.send(error);
+        };
+
+        try {
+            res.send(result);
+        } catch (error) {
+            res.send(error);
+        };
+        
+    })
+    
+};
+
+const update_book = async (req, res) => {
+
+    await Book.updateOne({title: req.body.title}, {title: 'Devlet', author: 'Platon', pageNumber: 357})
+        .then(async ()=>{
+            await Book.find({title: 'Devlet'})
+                .then(book => res.send(book))
+                .catch(e => res.send(e));
+        })
+        .catch(e => res.send(e));
+};
+
+const delete_book = async (req, res) => {
+
+    await Book.deleteOne({title: req.body.title})
+        .then(async () => {
+            await Book.find({}).then(books => res.send(books)).catch(e => res.send(e));
+        });
+};
+
+module.exports = {
+    list_all_book,
+    save_book,
+    update_book,
+    delete_book
+};
--- api/routes/bookDirectoryRoutes.js
+++ api/routes/bookDirectoryRoutes.js
@@ -1,73 +1,14 @@
 var express = require('express');
-var Book = require('../models/bookDirectoryModel');
+var {list_all_book, save_book, update_book, delete_book} = require('../controllers/bookDirectoryController');
 
 var router = express.Router();
 
-router.get('/list_all_book', async (req, res) => {
+router.get('/list_all_book', list_all_book);
 
+router.post('/save_book', save_book);
 
-    //sorting
-    /*await Book.find({}).sort({pageNumber: -1}).exec((error, docs) => {
-        if(error){
-            return res.send(error);
-        };
-        res.send(docs);
-    });*/
+router.put('/update_book', update_book);
 
-    //filtering
-    /*var title = req.query.title; 
-    var author = req.query.author;
-    await Book.find({title, author}).then(books => res.send(books)).catch(e => res.send(e));*/
-
-    //limit
-    /*var limit = parseInt(req.query.limit);
-    await Book.find({}).limit(limit).then(books => res.send(books)).catch(e => res.send(e));*/
-
-    await Book.find({}).then(books => res.send(books)).catch(e => res.send(e));
-});
-
-router.post('/save_book', async (req, res) => {
-
-    const book = new Book({
-        title: req.body.title,
-        author: req.body.author,
-        pageNumber: req.body.pageNumber
-    });
-
-    await book.save((error, result) => {
-
-        if (error){
-            return res.send(error);
-        };
-
-        try {
-            res.send(result);
-        } catch (error) {
-            res.send(error);
-        };
-        
-    })
-    
-});
-
-router.put('/update_book', async (req, res) => {
-
-    await Book.updateOne({title: req.body.title}, {title: 'Devlet', author: 'Platon', pageNumber: 357})
-        .then(async ()=>{
-            await Book.find({title: 'Devlet'})
-                .then(book => res.send(book))
-                .catch(e => res.send(e));
-        })
-        .catch(e => res.send(e));
-});
-
-router.delete('/delete_book', async (req, res) => {
-
-    await Book.deleteOne({title: req.body.title})
-        .then(async () => {
-            await Book.find({}).then(books => res.send(books)).catch(e => res.send(e));
-        });
-});
-
+router.delete('/delete_book', delete_book);
 
 module.exports = router;
